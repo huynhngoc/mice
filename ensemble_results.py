600f6443040422fffddc69d110a59c0dd0cfb564
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2023-11-20 15:39:47 +0100

    Added pipeline

---
+++ ensemble_results.py
@@ -0,0 +1,93 @@
+"""
+Example of running a single experiment of unet in the head and neck data.
+The json config of the main model is 'examples/json/unet-sample-config.json'
+All experiment outputs are stored in '../../hn_perf/logs'.
+After running 3 epochs, the performance of the training process can be accessed
+as log file and perforamance plot.
+In addition, we can peek the result of 42 first images from prediction set.
+"""
+
+import customize_obj
+# import h5py
+# from tensorflow.keras.callbacks import EarlyStopping
+import tensorflow as tf
+from deoxys.experiment import DefaultExperimentPipeline
+# from deoxys.model.callbacks import PredictionCheckpoint
+# from deoxys.utils import read_file
+import argparse
+# import os
+# import numpy as np
+# from pathlib import Path
+# from comet_ml import Experiment as CometEx
+from sklearn import metrics
+from sklearn.metrics import matthews_corrcoef
+
+
+class Matthews_corrcoef_scorer:
+    def __call__(self, *args, **kwargs):
+        return matthews_corrcoef(*args, **kwargs)
+
+    def _score_func(self, *args, **kwargs):
+        return matthews_corrcoef(*args, **kwargs)
+
+
+try:
+    metrics.SCORERS['mcc'] = Matthews_corrcoef_scorer()
+except:
+    pass
+try:
+    metrics._scorer._SCORERS['mcc'] = Matthews_corrcoef_scorer()
+except:
+    pass
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("log_base")
+    parser.add_argument("name_list")
+    parser.add_argument("--merge_name", default='merge', type=str)
+    parser.add_argument("--mode", default='ensemble', type=str)
+    parser.add_argument("--meta", default='patient_idx,slice_idx', type=str)
+    parser.add_argument(
+        "--monitor", default='AUC', type=str)
+    parser.add_argument(
+        "--monitor_mode", default='max', type=str)
+
+    args, unknown = parser.parse_known_args()
+
+    log_path_list = [args.log_base +
+                     name for name in args.name_list.split(',')]
+    log_base_path = args.log_base + args.merge_name
+
+    if args.mode == 'ensemble':
+        print('Ensemble test results from this list', log_path_list)
+    else:
+        print('Concatenate test results from this list', log_path_list)
+
+    print('Merged results are save to', log_base_path)
+
+    def binarize(targets, predictions):
+        return targets, (predictions > 0.5).astype(targets.dtype)
+
+    def flip(targets, predictions):
+        return 1 - targets, 1 - (predictions > 0.5).astype(targets.dtype)
+
+    pp = customize_obj.EnsemblePostProcessor(
+        log_base_path=log_base_path,
+        log_path_list=log_path_list,
+        map_meta_data=args.meta.split(',')
+    )
+
+    if args.mode == 'ensemble':
+        pp.ensemble_results()
+    else:
+        pp.concat_results()
+
+    pp.calculate_metrics(
+        metrics=['AUC', 'roc_auc', 'BinaryCrossentropy',
+                 'BinaryAccuracy', 'mcc', 'f1', 'f1'],
+        metrics_sources=['tf', 'sklearn',
+                         'tf', 'tf', 'sklearn', 'sklearn', 'sklearn'],
+        process_functions=[None, None, None, None, binarize, binarize, flip],
+        metrics_kwargs=[{}, {}, {}, {}, {}, {}, {'metric_name': 'f1_0'}]
+    )
---
+++ slurm_pretrain_pipeline.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH --ntasks=1               # 1 core(CPU)
+#SBATCH --nodes=1                # Use 1 node
+#SBATCH --job-name=mice_pretrain   # sensible name for the job
+#SBATCH --mem=64G                 # Default memory per CPU is 3GB.
+#SBATCH --partition=gpu # Use the verysmallmem-partition for jobs requiring < 10 GB RAM.
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=16
+#SBATCH --mail-user=ngochuyn@nmbu.no # Email me when job is done.
+#SBATCH --mail-type=FAIL
+#SBATCH --output=outputs/pretrain-%A.out
+#SBATCH --error=outputs/pretrain-%A.out
+
+# If you would like to use more please adjust this.
+
+## Below you can put your scripts
+# If you want to load module
+module load singularity
+
+## Code
+# If data files aren't copied, do so
+#!/bin/bash
+if [ $# -lt 3 ];
+    then
+    printf "Not enough arguments - %d\n" $#
+    exit 0
+    fi
+
+# if [ ! -d "$TMPDIR/$USER/CubiAI" ]
+#     then
+#     echo "Didn't find dataset folder. Copying files..."
+#     mkdir --parents $TMPDIR/$USER/CubiAI
+#     fi
+
+# for f in $(ls $PROJECTS/ngoc/CubiAI/datasets/*)
+#     do
+#     FILENAME=`echo $f | awk -F/ '{print $NF}'`
+#     echo $FILENAME
+#     if [ ! -f "$TMPDIR/$USER/CubiAI/$FILENAME" ]
+#         then
+#         echo "copying $f"
+#         cp -r $PROJECTS/ngoc/CubiAI/datasets/$FILENAME $TMPDIR/$USER/CubiAI/
+#         fi
+#     done
+
+
+echo "Finished seting up files."
+
+# Hack to ensure that the GPUs work
+nvidia-modprobe -u -c=0
+
+# Run experiment
+# export ITER_PER_EPOCH=128
+export NUM_CPUS=4
+export RAY_ROOT=$TMPDIR/$USER/ray
+singularity exec --nv deoxys.sif python experiment_binary.py $1 $PROJECTS/KBT/mice/perf/$2 --temp_folder $SCRATCH_PROJECTS/KBT/perf/$2 --epochs $3 ${@:4}
+
+
+
+fold_list=$3
+for fold in ${fold_list//,/ }
+do
+    singularity exec --nv deoxys.sif python experiment_binary.py $1_$fold.json $PROJECTS/KBT/mice/perf/$2_$fold --temp_folder $SCRATCH_PROJECTS/KBT/perf/$2_$fold --analysis_folder $SCRATCH_PROJECTS/KBT/perf/$2_$fold --epochs $4 ${@:5}
+done
+
+test_fold_idx=${fold: -1}
+
+singularity exec --nv deoxys.sif python -u ensemble_results.py $PROJECTS/KBT/mice/perf/$2_ $3 --merge_name test_fold$test_fold_idx
