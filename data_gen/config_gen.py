5631413f16a1fbb0753309ab45f51058bedc6910
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2023-11-20 17:29:26 +0100

    Added b2-b4 models

--- data_gen/config_gen.py
+++ data_gen/config_gen.py
@@ -35,3 +35,42 @@ for fn in filenames:
     new_fn = fn.replace('0001', '005')
     with open(f'config/eff/{new_fn}', 'w') as f:
         json.dump(config, f)
+
+
+
+# ===========================================================================
+filenames = [fn for fn in os.listdir('config/eff') if fn.startswith('b1_')]
+assert len(filenames) == 20 * 4
+
+for fn in filenames:
+    with open(f'config/eff/{fn}', 'r') as f:
+        config = json.load(f)
+    print(config['architecture']['class_name'])
+
+
+
+for fn in filenames:
+    with open(f'config/eff/{fn}', 'r') as f:
+        config = json.load(f)
+    config['architecture']['class_name'] = 'B2'
+    new_fn = fn.replace('b1', 'b2')
+    with open(f'config/eff/{new_fn}', 'w') as f:
+        json.dump(config, f)
+
+
+for fn in filenames:
+    with open(f'config/eff/{fn}', 'r') as f:
+        config = json.load(f)
+    config['architecture']['class_name'] = 'B3'
+    new_fn = fn.replace('b1', 'b3')
+    with open(f'config/eff/{new_fn}', 'w') as f:
+        json.dump(config, f)
+
+
+for fn in filenames:
+    with open(f'config/eff/{fn}', 'r') as f:
+        config = json.load(f)
+    config['architecture']['class_name'] = 'B4'
+    new_fn = fn.replace('b1', 'b4')
+    with open(f'config/eff/{new_fn}', 'w') as f:
+        json.dump(config, f)
