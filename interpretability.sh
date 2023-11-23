fcc849373335ed679dae31711e99c19c54c98dbc
Ngoc Huynh <huynhbaongoc0412@gmail.com>
2023-11-23 12:35:43 +0100

    Removed copying file scripts

--- interpretability.sh
+++ interpretability.sh
@@ -25,22 +25,22 @@ if [ $# -lt 2 ];
     exit 0
     fi
 
-if [ ! -d "$TMPDIR/$USER/hn_delin" ]
-    then
-    echo "Didn't find dataset folder. Copying files..."
-    mkdir --parents $TMPDIR/$USER/hn_delin
-    fi
+# if [ ! -d "$TMPDIR/$USER/hn_delin" ]
+#     then
+#     echo "Didn't find dataset folder. Copying files..."
+#     mkdir --parents $TMPDIR/$USER/hn_delin
+#     fi
 
-for f in $(ls $PROJECTS/ngoc/datasets/headneck/*)
-    do
-    FILENAME=`echo $f | awk -F/ '{print $NF}'`
-    echo $FILENAME
-    if [ ! -f "$TMPDIR/$USER/hn_delin/$FILENAME" ]
-        then
-        echo "copying $f"
-        cp -r $PROJECTS/ngoc/datasets/headneck/$FILENAME $TMPDIR/$USER/hn_delin/
-        fi
-    done
+# for f in $(ls $PROJECTS/ngoc/datasets/headneck/*)
+#     do
+#     FILENAME=`echo $f | awk -F/ '{print $NF}'`
+#     echo $FILENAME
+#     if [ ! -f "$TMPDIR/$USER/hn_delin/$FILENAME" ]
+#         then
+#         echo "copying $f"
+#         cp -r $PROJECTS/ngoc/datasets/headneck/$FILENAME $TMPDIR/$USER/hn_delin/
+#         fi
+#     done
 
 
 echo "Finished seting up files."
